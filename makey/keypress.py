664f2d32587ddc184c5b08ef95f81d14845604bb
unknown <kelly.ry4n@gmail>
2014-02-22 23:18:41 -0500

    working backend

--- makey/keypress.py
+++ makey/keypress.py
@@ -1,12 +1,33 @@
 import pythoncom, pyHook #pythoncom from pywin32
- 
-def OnKeyboardEvent(event):
+from pads import pads
 
-    a = chr(event.Ascii)
+def init(out_q):
+    ''' Starts up the listening thread and sets the queues'''
+
+    def OnKeyboardEvent(event):
+        # This contains the ascii value of a pad event
+
+        a = chr(event.Ascii)
+
+        try:
+            pad = pads[a]
+        except KeyError:
+            return
         
-    return True
+        # This queue is the input for the piezo module
+        # This value will eventually be passed back to main, after
+        # the note velocity is calculated for the input
+        # print pad
+        try:
+            out_q.put(pad)
+        except TypeError:
+            pass
 
-hm = pyHook.HookManager()
-hm.KeyDown = OnKeyboardEvent
-hm.HookKeyboard()
-pythoncom.PumpMessages()
+
+    def listen():
+        hm = pyHook.HookManager()
+        hm.KeyDown = OnKeyboardEvent
+        hm.HookKeyboard()
+        pythoncom.PumpMessages()
+
+    listen()
