0669cb46c36480f00201381c892e1b085a0f2236
Victor Reyes <haveon3@gmail.com>
2014-02-23 12:48:44 -0500

    It Works!

--- gui/YES.py
+++ gui/YES.py
@@ -1,5 +1,18 @@
 import pyglet
 from pyglet.window import key
+from pyglet.media import Player
+import random
+import sys
+sys.path += [sys.path[0] + '../../']
+from piezo.PiezoThreading import VolumeThread
+from Queue import Queue
+
+in_q = Queue()
+out_q = Queue()
+
+vol = VolumeThread(in_q,out_q)
+vol.start()
+
 
 window = pyglet.window.Window();
 
@@ -15,22 +28,28 @@ ohh = pyglet.media.StaticSource(pyglet.media.load('./audio/ohh/ohh-1.mp3'));
 crash = pyglet.media.StaticSource(pyglet.media.load('./audio/crash/crash-1.mp3'));
 ride = pyglet.media.StaticSource(pyglet.media.load('./audio/ride/ride-1.mp3'));
 
+drums = {
+	key.W:snare,
+	key.A:tom1,
+	key.S:tom2,
+	key.D:kick,
+	key.F:chh,
+	key.G:crash,
+	key.SPACE:ride
+}
+
 @window.event
 def on_key_press(symbol, modifiers):
-	if symbol == key.W:
-		snare.play();
-	elif symbol == key.A:
-		tom1.play();
-	elif symbol == key.S:
-		tom2.play();
-	elif symbol == key.D:
-		kick.play();
-	elif symbol == key.F:
-		chh.play();
-	elif symbol == key.G:
-		crash.play();
-	elif symbol == key.SPACE:
-		ride.play();
+	p = Player()
+
+	p.queue(drums[symbol])
+	in_q.put(None)
+	v = out_q.get(block=True)[1]
+	print v
+	p.volume = v
+	print p.volume
+	p.play()
+	# snare.play();
 
 @window.event
 def on_draw():
